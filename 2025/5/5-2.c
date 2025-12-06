2098148bc071cb9da01f1dabef33c0235911cb3e
V Scheffler <vonscheffler@protonmail.com>
2025-12-06 12:47:36 -0500

    Part two solution

--- 2025/5/5-2.c
+++ 2025/5/5-2.c
@@ -1,56 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct RANGE_T {
 	unsigned long lower;
 	unsigned long upper;
 } RANGE_T;
 
+int compar(const void *a, const void *b) { 
+	unsigned long a_low = ((RANGE_T *)a)->lower, b_low = ((RANGE_T *)b)->lower;
+
+	if (a_low < b_low) return -1;
+	if (a_low > b_low) return 1;
+
+	return 0;	
+}
+
 int main(int argc, char **argv) {
 	char *line_ptr = NULL, *end_ptr;
-	size_t n = 0;
-	int num_range = 0;
+	size_t n = 0, num_range = 0;
 	RANGE_T *ranges = NULL;
-	short found;
-	unsigned long lower, upper, sum = 0;
 	FILE *in_file = fopen(argv[1], "r");
 
 	while (getline(&line_ptr, &n, in_file) > 1) {
-		lower = strtoul(line_ptr, &end_ptr, 10);
-		upper = strtoul(end_ptr + 1, NULL, 10);
-
-		found = 0;
-
-		for (int i = 0; i < num_range; i++) {
-			if (lower >= ranges[i].lower && lower <= ranges[i].upper){
-				lower = ranges[i].upper + 1;
-				found = 1;
-			}
-			if (upper >= ranges[i].lower && upper <= ranges[i].upper) {
-				upper = ranges[i].lower - 1;
-				found = 1;
-			}
-			
-			if (upper < lower) break;
-			
-			if (found) {
-				found = 0;
-				i = -1;
-			}
-		}
-
-		printf("Lower: %lu\tUpper: %lu\t", lower, upper);
-		if (upper >= lower) {
-			sum += upper - lower + 1;
-			ranges = realloc(ranges, sizeof(*ranges) * ++num_range);
-			ranges[num_range - 1].lower = lower;
-			ranges[num_range - 1].upper = upper;
-		}
-		printf("Sum: %lu\n\n", sum);
+		ranges = realloc(ranges, sizeof(*ranges) * ++num_range);
+		ranges[num_range - 1].lower = strtoul(line_ptr, &end_ptr, 10);
+		ranges[num_range - 1].upper = strtoul(end_ptr + 1, NULL, 10);
 	}
 
 	fclose(in_file);
 	free(line_ptr);
+
+	qsort(ranges, num_range, sizeof(*ranges), &compar);
+
+	for (size_t i = 0; i < num_range - 1; ) {
+		if (ranges[i].upper >= ranges[i + 1].lower) {
+			if (ranges[i].upper < ranges[i + 1].upper) ranges[i].upper = ranges[i + 1].upper;
+			if (i < num_range - 2) 
+				memmove(ranges + i + 1, ranges + i + 2, sizeof(*ranges) * (num_range - i - 2));
+			num_range--;
+		}
+		else i++;
+	}
+
+	unsigned long sum = 0;
+
+	for (size_t i = 0; i < num_range; i++) sum += ranges[i].upper - ranges[i].lower + 1;
+
 	free(ranges);
 
 	printf("Sum = %lu\n", sum);
